5f6a4e1dddce35f04f8658ce3ae45a20ad05e1f5
prgmr99 <johnyeom24@naver.com>
2024-05-29 20:53:20 +0900

    test: cicd

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send("Hello ACCKWU!!");
+  res.send("YOU YOU YOU YOU YOU YOU YOU YOU MAGNETIC~~~!!");
 });
 
 app.listen(5002);
